e7748f36e07cc09b6307b10ee7e8bf57307e44e8
Cynthia <cynthia2048@proton.me>
2025-10-01 23:00:17 +0530

    Add new extractors for Hstrea.moe and HentaiHaven

---
+++ yt_dlp_plugins/extractor/hentaihaven.py
@@ -0,0 +1,55 @@
+import re
+import base64
+
+from yt_dlp.extractor.common import InfoExtractor, ExtractorError
+from yt_dlp.utils import multipart_encode
+
+class HentaiHavenIE(InfoExtractor):
+    _VALID_URL = r'https?://hentaihaven\.(co|com|xxx)/video/(?P<id>[\w\-_]+).*'
+    _VIDEO_TITLE_RE = r'chapter-heading" class="h3">([^<]+)'
+    _HH_EMBED_RE = r'[^"]+/wp-content/plugins/player-logic/player\.php[^"]+'
+    _CIPHER_MAP = dict(zip(b'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm',
+                           b'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'))
+
+    def _decipher_sec_token(self, s):
+        b = s.encode('ascii')
+
+        for _ in range(3):
+            # Map the characters which are part of the cipher, leave the rest.
+            b = bytes(map(lambda c: self._CIPHER_MAP.get(c) or c, b))
+            b = base64.b64decode(b)
+
+        return b.decode('ascii')
+
+    def _real_extract(self, url):
+        video_id = self._match_id(url)
+
+        hh_page = self._download_webpage(url, video_id)
+
+        video_title = self._html_search_regex(self._VIDEO_TITLE_RE, hh_page, 'title')
+        embed_url = self._search_regex(self._HH_EMBED_RE, hh_page, 'embed', group=0)
+        
+        hh_page = self._download_webpage(embed_url, video_id)
+        
+        cipher_text = self._html_search_meta('x-secure-token', hh_page).lstrip('sha512-')       
+        interim = self._parse_json(self._decipher_sec_token(cipher_text), video_id)
+        api_url = '{}/api.php'.format(interim['uri'])
+        payload = {
+            'action': 'zarat_get_data_player_ajax',
+            'a': interim['en'],
+            'b': interim['iv']
+        }
+        raw_payload, mime = multipart_encode(payload)
+        
+        hh_res = self._download_json(api_url, video_id, headers={'Content-Type': mime},
+                                     data=raw_payload)
+
+        if hh_res['status'] is False:
+            raise ExtractorError('Unable to extract JWPlayer data',
+                                 video_id=video_id, ie=HentaiHavenIE.ie_key())
+
+        result = self._parse_jwplayer_data(hh_res['data'], video_id, require_title=False)
+        result['title'] = video_title
+
+        return result
+
---
+++ yt_dlp_plugins/extractor/hstream.py
@@ -0,0 +1,53 @@
+import re
+import json
+import urllib.parse
+
+from yt_dlp.extractor.common import InfoExtractor
+
+
+class HstreamIE(InfoExtractor):
+    _VALID_URL = r'https?://hstream\.moe/hentai/(?P<id>[a-z0-9\-]+)'
+    _E_ID = r'e_id" type="hidden" value="([^"]*)'
+    # TODO add _TESTS
+
+    def _extract_cookie(self, name):
+        for cookie in self._downloader.cookiejar:
+            if cookie.name == name:
+                return cookie.value
+
+    def _real_extract(self, url):
+        # NOTE This has no use in the API itself; just a part of the webpage URL.
+        video_id = self._match_id(url)
+
+        page = self._download_webpage(url, video_id)
+        e_id = self._search_regex(self._E_ID, page, 'e_id')
+
+        payload = json.dumps({'episode_id': e_id})
+        xsrf_token = self._extract_cookie('XSRF-TOKEN')
+
+        video = self._download_json('https://hstream.moe/player/api', video_id, headers={
+                    'Content-Type': 'application/json',
+                    'Referer': url,
+                    'X-Requested-With': 'XMLHttpRequest',
+                    'X-Xsrf-Token': urllib.parse.unquote(xsrf_token)
+                }, data=payload.encode('utf-8'))
+
+        # NOTE Although all CDNs essentially provide same resources, based on the client's
+        # country, the speeds may differ. Before finializing, the behaviour of the official
+        # website should be consulted, although this seems to suffice for now. 
+        cdn_url = '{}/{}'.format(video['stream_domains'][0], video['stream_url'])
+        formats = []
+        
+        for res in ('720', '1080', '2160'):
+            manifest_url = '{}/{}/manifest.mpd'.format(cdn_url, res)
+            mpd_formats = self._extract_mpd_formats(manifest_url, video_id, mpd_id=res)
+
+            formats.extend(mpd_formats)
+
+        poster_url = '{}/{}'.format('https://hstream.moe', video.get('poster'))
+        return {
+            'id': e_id,
+            'title': video.get('title'),
+            'thumbnail': poster_url,
+            'formats': formats
+        }
